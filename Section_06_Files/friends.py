90dc1ec31e0310ebd995a2b4f8ce558e5f5f1815
Gabriel Montoya <gabi.montoya@gmail.com>
2018-04-01 14:47:27 +0200

    Section_06 L72 - Copiando files

--- Section_06_Files/friends.py
+++ Section_06_Files/friends.py
@@ -1,39 +1,3 @@
-# Section 06: Files in Python
-
-[VOLVER a README.md](README.md)
-
-## Indice
-
-* [Files (Open - read - write)](#files-open---read---write)
-* [Copiar archivos](#copiar-archivos)
-
-## Files (Open - read - write)
-
-* Modo: read (``'r'``)
-```python
-my_file = open('data.txt', 'r')
-file_content = my_file.read()
-my_file.close()
-
-print(file_content)
-```
-
-* Modo: write (``'w'``)
-
-```python
-user_name = input('Enter your name: ')
-
-my_file_writing = open('data.txt', 'w')
-my_file_writing.write(user_name)
-my_file_writing.close()
-```
->**Nota:** El modo ``w`` sobreescribe el contenido del archivo.
-
-[Video: Files in Python](https://www.udemy.com/the-complete-python-course/learn/v4/t/lecture/9445280?start=0)
-
-## Copiar archivos
-
-```python
 # Ask the user for a list of 3 friends
 # For each friend, we'll tell the user whether they are nearby
 # For each nearby friend, we'll save their name to 'nearby_friends.txt'
@@ -58,6 +22,10 @@ if __name__ == '__main__':
 
     nearby_friends_file.close()
 
-```
 
-[Video: Copiar archivos](https://www.udemy.com/the-complete-python-course/learn/v4/t/lecture/9445282?start=0)
+
+
+
+
+
+
