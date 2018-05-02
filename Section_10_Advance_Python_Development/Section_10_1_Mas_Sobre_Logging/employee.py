2d57702602fa834431fc12a76c0e6ac08634ad62
montoya <gabi.montoya@gmail.com>
2018-05-02 15:48:39 +0200

    section_10.1: Mas sobre Logging - FileHandlers, loggers and formatters

---
+++ Section_10_Advance_Python_Development/Section_10_1_Mas_Sobre_Logging/employee.py
@@ -0,0 +1,32 @@
+import logging
+
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler('employee.log')
+logger.addHandler(file_handler)
+logger.setLevel(logging.INFO)
+
+formater = logging.Formatter('%(asctime)s: %(levelname)s: %(name)s: %(message)s')
+file_handler.setFormatter(formater)
+
+
+class Employee:
+    """A sample Employee class"""
+
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+
+        logger.info(f'Created Employee: { self.fullname } - {self.email}')
+
+    @property
+    def email(self):
+        return '{}.{}@email.com'.format(self.first, self.last)
+
+    @property
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+
+emp_1 = Employee('John', 'Smith')
+emp_2 = Employee('Corey', 'Schafer')
+emp_3 = Employee('Jane', 'Doe')
